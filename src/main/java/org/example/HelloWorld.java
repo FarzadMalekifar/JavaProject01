dafe014e8779beff1781066c107f93530620a404
Farzad Malekifar <73961833+FarzadMalekifar@users.noreply.github.com>
2023-10-31 13:36:57 +0100

    initial version

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        double a ;
+        int b = 42;
+        a =Double.valueOf(b);
+        System.out.println("Hello world " + a);
+    }
+}
